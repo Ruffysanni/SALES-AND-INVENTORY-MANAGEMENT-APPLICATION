4b264a15dda501a74b3c366e3ca8c87cf58f3533
Rufai Sanni <sruffy07@gmail.com>
2024-05-23 15:00:43 +0100

    initial commit

---
+++ INGRYD_CRM/src/test/java/com/INGRYD/INGRYD_CRM/IngrydCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.INGRYD.INGRYD_CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngrydCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
