faeab6dfa9cc3a4207da42143dffe046539fe7a9
Rufai Sanni <sruffy07@gmail.com>
2024-05-26 22:22:24 +0100

    add annotations to the repository interfaces created

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ItemRepository.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.INGRYD.INGRYD_CRM.repository;
 
 import com.INGRYD.INGRYD_CRM.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ProductRepository.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.INGRYD.INGRYD_CRM.repository;
 
 import com.INGRYD.INGRYD_CRM.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByProductName(String name);
 }
