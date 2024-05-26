64514e3b85242181d9b5bfb216c5885abada2779
smith <smithbabawande@gmail.com>
2024-05-26 12:19:01 +0100

    code refactored

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/CustomerRepository.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/CustomerRepository.java
@@ -3,6 +3,6 @@ package com.INGRYD.INGRYD_CRM.repository;
 import com.INGRYD.INGRYD_CRM.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository <Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findByCustomerName(String name);
 }
