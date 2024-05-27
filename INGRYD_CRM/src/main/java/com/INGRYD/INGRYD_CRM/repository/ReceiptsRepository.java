7d95c4a00fa6eea120032a4de2c77a99a11b4181
smith <smithbabawande@gmail.com>
2024-05-27 08:14:06 +0100

    Repository annotated

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ReceiptsRepository.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/repository/ReceiptsRepository.java
@@ -2,7 +2,9 @@ package com.INGRYD.INGRYD_CRM.repository;
 
 import com.INGRYD.INGRYD_CRM.model.Receipts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReceiptsRepository extends JpaRepository<Receipts, Long> {
   Receipts findByReceiptId(Long id);
 }
