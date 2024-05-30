5ac4b961b6036a36006fd5ffadbba2d3297ac4ce
smith <smithbabawande@gmail.com>
2024-05-30 10:02:57 +0100

    CustomerService completed

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/service/CustomerService.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/service/CustomerService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,9 +23,39 @@ public class CustomerService {
     }
 
     //getCustomerById
+    public ResponseEntity<Customer> getCustomerById (@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id).get(), HttpStatus.OK);
+    }
+    //getCustomerName
+    public ResponseEntity<Customer> getCustomerName(@RequestParam String name){
+        return new ResponseEntity<>(customerRepository.findByCustomerName(name), HttpStatus.OK);
+    }
+
+    //postCustomer
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return  new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
 
     //updateCustomer
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, Customer replacementDetails){
+        Customer customer = customerRepository.findById(id).get();
+        customer.setName(replacementDetails.getName());
+        customer.setEmail(replacementDetails.getEmail());
+        customer.setPhoneNumber(replacementDetails.getPhoneNumber());
+        customer.setAddress(replacementDetails.getAddress());
+
+        return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.OK);
+    }
+
 
     //deleteCustomer
+    public ResponseEntity<Customer> deleteCustomer(Long id){
+        Customer customer = customerRepository.findById(id).get();
+        customerRepository.deleteById(id);
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
 
 }
