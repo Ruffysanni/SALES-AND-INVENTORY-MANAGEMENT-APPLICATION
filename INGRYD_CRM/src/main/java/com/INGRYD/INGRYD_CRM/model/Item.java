27565f89216727dd8476449c005696869e66eb3a
Rufai Sanni <sruffy07@gmail.com>
2024-05-31 12:45:55 +0100

    Add services for the Item and Product

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/model/Item.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/model/Item.java
@@ -49,15 +49,15 @@ public class Item {
     public Item() {
     }
 
-    public Item(Long item_id, Long sale_id, Long product_id, Double quantity, Double unitPrice, Sale sale, Product product) {
-        this.item_id = item_id;
-        this.sale_id = sale_id;
-        this.product_id = product_id;
-        this.quantity = quantity;
-        this.unitPrice = unitPrice;
-        this.sale = sale;
-        this.product = product;
-    }
+//    public Item(Long item_id, Long sale_id, Long product_id, Double quantity, Double unitPrice, Sale sale, Product product) {
+//        this.item_id = item_id;
+//        this.sale_id = sale_id;
+//        this.product_id = product_id;
+//        this.quantity = quantity;
+//        this.unitPrice = unitPrice;
+//        this.sale = sale;
+//        this.product = product;
+//    }
 
     public Long getSale_id() {
         return sale_id;
@@ -67,13 +67,6 @@ public class Item {
         return item_id;
     }
 
-//    public void setSale_id(Long sale_id) {
-//        this.sale_id = sale_id;
-//    }
-
-    //    public void setProduct_id(Long product_id) {
-//        this.product_id = product_id;
-//    }
     public Long getProduct_id() {
         return product_id;
     }
---
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/service/ItemService.java
@@ -0,0 +1,44 @@
+package com.INGRYD.INGRYD_CRM.service;
+
+import com.INGRYD.INGRYD_CRM.model.Item;
+import com.INGRYD.INGRYD_CRM.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems(){
+        return itemRepository.findAll();
+    }
+    public Item getItemById(Long id){
+        return itemRepository.findById(id).get();
+    }
+    public Item postNewItem(Item item){
+        return itemRepository.save(item);
+    }
+    public Item updateItem(Item itemToUpdate, Long id){
+        // CHeck if tem to be update is available in the database
+        Item itemFromDb = itemRepository.findById(id).get();
+
+        // Update the new item with the existing one
+        if(itemFromDb != null){
+            itemToUpdate.setQuantity(itemFromDb.getQuantity());
+            itemToUpdate.setUnitPrice(itemFromDb.getUnitPrice());
+        }
+        return itemRepository.save(itemToUpdate);
+    }
+
+    public void deleteItem(Item item,  Long id){
+        // Check if tem to be update is available in the database
+        Item itemFromDb = itemRepository.findById(id).get();
+        // Delete the new item from the repository
+        if(itemFromDb != null){
+            itemRepository.delete(item);
+        }
+    }
+}
