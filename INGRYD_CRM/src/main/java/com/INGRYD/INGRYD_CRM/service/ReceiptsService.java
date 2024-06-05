5e5c8ca8a7eebf16ee64240e26c3c7512fc41ac8
smith <smithbabawande@gmail.com>
2024-06-05 09:15:05 +0100

    receipt pdf logic added

--- INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/service/ReceiptsService.java
+++ INGRYD_CRM/src/main/java/com/INGRYD/INGRYD_CRM/service/ReceiptsService.java
@@ -2,12 +2,18 @@ package com.INGRYD.INGRYD_CRM.service;
 
 import com.INGRYD.INGRYD_CRM.model.Receipts;
 import com.INGRYD.INGRYD_CRM.repository.ReceiptsRepository;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -44,17 +50,44 @@ public class ReceiptsService {
     public ResponseEntity<Receipts> createReceipt(Payment payment) {
         paymentService.createPayment(payment);
         Receipts receipts = new Receipts();
+        createPDF(receipts);
 
         Receipts savedReceipt = receiptsRepository.save(receipts);
         return new ResponseEntity<>(savedReceipt, HttpStatus.CREATED);
     }
 
+    private void createPDF(Receipts receipts) throws IOException{
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+
+            contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.valueOf("Helvetica-Bold")), 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(25, 700);
+            contentStream.showText("Receipt Details");
+            contentStream.endText();
+
+            contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.valueOf("Helvetica")), 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(25, 650);
+            contentStream.showText("Amount: " + receipts.getAmount());
+            contentStream.newLine();
+            contentStream.showText("Narration: " + receipts.getNarration());
+            contentStream.endText();
+        }
+
+        document.save("receipt.pdf");
+        document.close();
+    }
 
     //Get Receipts by Date Range
     public ResponseEntity<List<Receipts>> getReceiptsByDateRange(LocalDate startDate, LocalDate endDate) {
         List<Receipts> receipts = receiptsRepository.findByReceiptDateBetween(startDate, endDate);
         return new ResponseEntity<>(receipts, HttpStatus.OK);
     }
+
 
 
 }
